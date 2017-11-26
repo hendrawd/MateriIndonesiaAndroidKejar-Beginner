dfeb30469fe2b43ddc1e9799e72b1009adbff28c
hendrawd <hendrawd88@gmail.com>
2017-11-26 13:41:47 +0700

    Add profile layout

--- app/src/main/java/company/helloworld/MainActivity.java
+++ app/src/main/java/company/helloworld/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frame_layout);
+        setContentView(R.layout.activity_image_view);
     }
 }
