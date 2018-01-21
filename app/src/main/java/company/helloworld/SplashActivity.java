1c6e8298c2f804803191d4804ed1d2f0dc5532a0
hendrawd <hendrawd88@gmail.com>
2018-01-21 15:49:19 +0700

    Add example for splash activity

---
+++ app/src/main/java/company/helloworld/SplashActivity.java
@@ -0,0 +1,28 @@
+package company.helloworld;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intentUntukPindahKeActivityUtama = new Intent(
+                                SplashActivity.this,
+                                MainActivity.class
+                        );
+                        startActivity(intentUntukPindahKeActivityUtama);
+                    }
+                }, 2_000
+        );
+    }
+}
