08c20d407ddafa3788bec2180faf31ec908f2506
Hendra Wijaya Djiono <hendraz_88@yahoo.co.id>
2018-01-22 18:33:24 +0700

    Update layout name of MainActivity.java

--- app/src/main/java/company/helloworld/MainActivity.java
+++ app/src/main/java/company/helloworld/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_main);
     }
 
     // ini namanya method
